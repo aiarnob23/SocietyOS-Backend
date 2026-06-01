8f3ad5c4eda9f06024a4a6977795149ac16f0844
aiarnob23 <aiarnob23@gmail.com>
2026-06-02 02:54:22 +0600

    auth module partially updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UsersModule, AuthModule],
-  controllers: [AppController, UsersController, AuthController],
-  providers: [AppService, AuthService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
