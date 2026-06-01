10e2dba429d605e9b56814d2f13db23f1f562a86
aiarnob23 <aiarnob23@gmail.com>
2026-06-02 02:48:45 +0600

    auth module partially updated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dto/register.dto';
+import { User } from 'src/generated/prisma/client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+    ) { }
+
+    async register(dto: RegisterDto): Promise<User> {
+        const user = await this.usersService.createUser({
+            email: dto.email,
+        });
+        return user;
+    }
+}
