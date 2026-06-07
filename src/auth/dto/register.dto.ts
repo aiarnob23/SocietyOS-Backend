0808b178352d90476378c4c60c3cbf9406c11a1d
aiarnob23 <aiarnob23@gmail.com>
2026-06-07 20:18:58 +0600

    register dto updated

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,6 +1,44 @@
-import { IsEmail } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsEmail, IsEnum, isNotEmpty, IsOptional, IsPhoneNumber, IsString, isString, Matches, MaxLength, MinLength } from "class-validator";
+import { UserRole } from "src/generated/prisma/enums";
 
 export class RegisterDto {
+    @Transform(({ value }) => value?.trim().toLowerCase())
     @IsEmail()
     email: string;
+
+    @IsOptional()
+    @IsPhoneNumber()
+    phone?: string;
+
+    @Transform(({ value }) => value?.trim())
+    @IsString()
+    @MinLength(2)
+    @MaxLength(50)
+    firstName: string;
+
+    @Transform(({ value }) => value?.trim())
+    @IsString()
+    @IsOptional()
+    @MinLength(2)
+    @MaxLength(50)
+    lastName?: string;
+
+    @IsEnum(UserRole)
+    role: UserRole;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(72)
+    @Matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+        {
+            message:
+                'Password must contain at least one uppercase letter, one lowercase letter and one number',
+        },
+    )
+    password: string;
+
+    @IsOptional()
+    communityId?: number;
 }
